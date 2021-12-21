3f6c13001edcffca2863a811d3d3d9c6a43cdd92
Charlie Pradeep <charliepradeep@gmail.com>
2021-12-21 15:44:07 +0530

    Charrlie Commit 1-N

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -319,7 +319,7 @@ public class BaseTest {
 		 // utils.log().info("load " + xmlFileName);
 		 // stringsis = getClass().getClassLoader().getResourceAsStream(xmlFileName);
 		 // setStrings(utils.parseStringXML(stringsis));
-		  String directoryPath = System.getenv("$$BITRISE_APK_PATH");
+		  String directoryPath = System.getenv("$BITRISE_APK_PATH");
 		  String apppath = props.getProperty(directoryPath);
 		  
 			DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
